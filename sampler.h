ce7ba2a79e1dc2019a532fda1a4c6920dfeca811
Gaetino <gaith26740@yahoo.fr>
2013-04-15 15:31:20 +0300

    add comments

--- sampler.h
+++ sampler.h
@@ -1,7 +1,7 @@
 #include <arduino.h>
 
 const int chipSelect = 10;         // For Due, the SS of SD Card is on pin 10. See SdFat library
-const int bufsize = 1024;          // buffer size in bytes.
+const int bufsize = 1024;          // buffer size in bytes. You can change this data.
 
 // Header of a wave file.
 // Num_channels will tell us if we are in stereo (2) or mono (1)
@@ -40,11 +40,11 @@ public:
    uint32_t decrease;          // decrease speed
    uint16_t samplenote;        // sample note
    
-   SdFile myFile;
-   
-   wave_header header;
+   SdFile myFile;              // The wave file
+   wave_header header;         // The wave header
   
-   
+
+// Init of the sampler object. Here you can put your default values.
    void init()
    {
      volsample = 0;
@@ -54,7 +54,8 @@ public:
      play = false;
      volglobal = 800;	 
    }
-   
+
+// Play the sample, giving volume and note (volume 0-1024; note 0-128)
    void splay(uint32_t vol, uint16_t note)
    {
      
@@ -62,7 +63,8 @@ public:
      volglobal = vol;
      samplenote=note;
    }
-   
+
+// load the wave header in the header data, and the beginning of the wave file in the buffer.
    void load(const char* samplename)
    {
      bufread = 0;
@@ -75,13 +77,15 @@ public:
      volsample = 0;
      decrease = endofsample-(2048*header.num_channels);
    }
-   
+
+//Stop playing the sample
    void sstop()
    {
      play=false;
      myFile.close();
    }
-   
+
+// Stop playing the sample, giving a note.
    void notestop(uint16_t note)
    {
      if(samplenote==note) 
@@ -90,17 +94,21 @@ public:
        endofsample = possample + header.num_channels*2048;  
      }
    }
-   
+
+// Set the volume of the sample
    void setVol(uint32_t vol)
    {
      volglobal = vol;
    }
-   
+
+//Set the end of the sample
    void setEnd(uint32_t theEnd)
    {
      endofsample = (endofsample*theEnd)>>7;
    }
-      
+
+// Fill the buffer if it has to be. 
+// This method must not be called in the sound generating loop.
    void buffill()
    {
      if(play)
@@ -115,30 +123,36 @@ public:
    }
    
    
-   
+// Compute the position of the next sample. Must be called in the sound generating loop, before the method "output()"   
    void next()
    {
      if(play)  
      {
-       indbuf += header.num_channels;
+       // If the wave file is stereo, we take only one side (left). 
+       indbuf += header.num_channels; 
        possample += (header.num_channels + header.num_channels);
-		 
+	
+       // The sample doesn't stop playing abruptly, because it can make some "clicks"
+       // So it's decreasing rapidly after we stop it (like a release of an enveloppe)
        if(possample>=decrease)
        {
          if(volsample>0) volsample -= 1;
        }
-		 
+
+      // The sample doesn't start abruptly. There is a quick attack to avoid "clicks"
       if(volsample<=volglobal&&possample<=1024)
       {
          volsample += 8;
       }
-		 
+	
+      // The reading of the file must stop at the end of file.
       if(possample>=(endofsample-1))
       {
          play = false;
          myFile.close();
       }
 
+      // If a buffer is finished to be read, we read the other buffer.
       if(indbuf>=bufsize)
       {
         if(bufread==0) bufread = 1;
@@ -148,7 +162,8 @@ public:
       }
     } 
   }
-   
+
+// Read the wave file at a position. Returns the volume (12 bits)
   uint32_t output()
   {
     uint32_t ret;
