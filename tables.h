f68d9513bb25fcd1fe0807d6d17d0436b2c93bfa
Gaetino <gaith26740@yahoo.fr>
2013-04-18 16:32:49 +0300

    change unsigned int to signed int

--- tables.h
+++ tables.h
@@ -4,9 +4,9 @@
 #define TICKS_PER_CYCLE (float)((float)SAMPLES_PER_CYCLE_FIXEDPOINT/(float)SAMPLE_RATE) 
 
 #define WAVE_SAMPLES 600 
-uint16_t nSineTable[WAVE_SAMPLES]; 
-uint16_t nSawTable[WAVE_SAMPLES]; 
-uint16_t nSqTable[WAVE_SAMPLES];
+int16_t nSineTable[WAVE_SAMPLES]; 
+int16_t nSawTable[WAVE_SAMPLES]; 
+int16_t nSqTable[WAVE_SAMPLES];
 
 #define MIDI_NOTES 128 
 uint32_t nMidiPhaseIncrement[MIDI_NOTES]; 
@@ -17,8 +17,7 @@ void createSineTable()
   for(uint32_t nIndex = 0;nIndex  < WAVE_SAMPLES;nIndex++) 
   { 
     // normalised to 12 bit range 0-4095 
-    nSineTable[nIndex] = (uint16_t)  (((1+sin(((2.0*PI)/WAVE_SAMPLES)*nIndex))*4095.0)/2); 
-    //Serial.println(nSineTable[nIndex]); 
+    nSineTable[nIndex] = (int16_t)  ((sin(((2.0*PI)/WAVE_SAMPLES)*nIndex))*2048.0);
   } 
 } 
 
@@ -26,7 +25,7 @@ void createSawTable()
 { 
   for(uint32_t nIndex = 0;nIndex  < WAVE_SAMPLES;nIndex++) 
   { 
-    nSawTable[nIndex] = (uint16_t)  (nIndex*(4095.0/600)); 
+    nSawTable[nIndex] = (uint16_t)  (nIndex*(4095.0/600)-2048); 
   } 
 } 
 
@@ -34,7 +33,7 @@ void createSqTable()
 { 
   for(uint32_t nIndex = 0; nIndex  < WAVE_SAMPLES; nIndex++) 
   { 
-    if(nIndex<(WAVE_SAMPLES/2)) nSqTable[nIndex] = 0;
-    if(nIndex>=(WAVE_SAMPLES/2)) nSqTable[nIndex] = 4095; 
+    if(nIndex<(WAVE_SAMPLES/2)) nSqTable[nIndex] = -2048;
+    if(nIndex>=(WAVE_SAMPLES/2)) nSqTable[nIndex] = 2047; 
   } 
 } 
